f518a5f4135f93c778c51471893c8873b500d515
Niko <nikokirste@hotmail.de>
2018-03-13 15:39:38 +0100

    added help intent

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var AlexaAppServer = require('alexa-app-server');
 
 var server = new AlexaAppServer({
 	httpsEnabled: false,
-	port: process.env.PORT || 80
+	port: process.env.PORT || 80,
+	debug: true,
+	verify: true
 });
 
 server.start();
