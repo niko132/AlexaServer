a4bca9d226669c7cac4c500dee0793bc906d4c18
Niko <nikokirste@hotmail.de>
2018-03-13 22:39:45 +0100

    .

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ var AlexaAppServer = require('alexa-app-server');
 var server = new AlexaAppServer({
 	httpsEnabled: false,
 	port: process.env.PORT || 80,
-	debug: true  /**,
+/*	debug: true, */
 	verify: true
-	**/
 });
 
 server.start();
