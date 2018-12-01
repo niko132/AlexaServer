f81d4baa95f8178e24a55936a395e229a0b5a809
niko132 <nikokirste@hotmail.de>
2018-12-01 19:50:21 +0100

    color

--- server.js
+++ server.js
@@ -10,31 +10,3 @@ var server = new AlexaAppServer({
 });
 
 server.start();
-
-var Canvas = require('canvas'),
-	Image = Canvas.Image,
-	canvas = new Canvas(200, 200),
-	ctx = canvas.getContext('2d'),
-	fs = require('fs');
- 
-ctx.font = '30px Impact';
-ctx.rotate(.1);
-ctx.fillText("Awesome!", 50, 100);
- 
-var te = ctx.measureText('Awesome!');
-ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.lineTo(50 + te.width, 102);
-ctx.stroke();
- 
-console.log('<img src="' + canvas.toDataURL() + '" />');
-
-var pngfile = fs.createWriteStream('test.png'),
-	stream = canvas.pngStream();
-stream.on('data', function(chunk) {
-	pngfile.write(chunk);
-});
-stream.on('end', function() {
-	console.log('saved png');
-});
